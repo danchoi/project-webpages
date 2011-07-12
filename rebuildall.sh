244f79e99e894c57cb1f3b083435e39357b1ad94
Daniel Choi <dhchoi@gmail.com>
2011-07-11 23:25:01 -0400

    rebuild script

---
+++ rebuildall.sh
@@ -0,0 +1,14 @@
+cd ~/p/hulu_automuter
+rake web
+cd ~/p/vmail
+rake web
+cd ~/p/vitunes
+rake web
+cd ~/p/ri_vim
+rake web
+cd ~/p/rb_nav
+rake web
+cd ~/p/soywiki
+rake web
+cd ~/p/vnews
+rake web
